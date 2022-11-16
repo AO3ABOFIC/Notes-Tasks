28e53490ae4c45c7219325266df74a606247f0ae
AO3ABOFIC <111791606+AO3ABOFIC@users.noreply.github.com>
2022-11-16 14:18:13 -0600

    Update 11-14-22-WCAG.py

--- 11-14-22-WCAG.py
+++ 11-14-22-WCAG.py
@@ -6,7 +6,8 @@ Please choose one of the options below, and press enter.
 2) When was WCAG released?
 3) Who works on WCAG?
 4) What is WCAG 2.1 in comparison to WCAG 2.0?
-5) SaaS accessibility preference
+5) 
+6) SaaS accessibility preference
 """)
 
 answer = "empty"
@@ -24,6 +25,8 @@ while (answer == "empty"):
   elif(userchoice == "4"):
     print("""WCAG 2.1 is an extension of WCAG 2.0, not a replacement. WCAG 2.1 added 17 more success criteria to WCAG 2.0, most of which were included to cover significant changes in regards to websites and content producers.""")
   elif(userchoice == "5"):
+    print("""I think that WCAG 3's accessibility guidelines of Guidance for more disability groups & Starts from user needs of disability groups instead of technical solutions are not worthy. These accessibility guidelines are extremely important as they move the focus towards making technology even more available for disabled people and accommodating for their needs rather than a technological focus that will persist naturally regardless of focus.""")
+  elif(userchoice == "6"):
     print("""My favorite accessibility menu on the different SaaS services is the second one. The second one has the most options to tailor the website to the user's needs and provides different colors for certain features, which sets it apart from the first one. While the third option also offers different colors, the colors are very bright, which can be distracting or impede a user's ability to navigate the site.""")
   else:
     print("That's not one of the options, silly!")
